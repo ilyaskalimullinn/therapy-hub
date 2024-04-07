87f6741ac83afbe6eb80247b07db622e582e3837
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2024-04-07 14:23:06 +0300

    add logout and profile view, add auth check in some views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegistrationView from '../views/RegistrationView.vue'
 import LoginView from '../views/LoginView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import LogoutView from '@/views/LogoutView.vue'
+import { useUserStore } from '@/stores/userStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,14 +26,50 @@ const router = createRouter({
       path: "/registration",
       name: "Registration",
       component: RegistrationView,
+      meta: {
+        requiresGuest: true
+      },
     },
     {
       path: "/login",
       name: "Login",
       component: LoginView,
+      meta: {
+        requiresGuest: true
+      },
     },
+    {
+      path: "/logout",
+      name: "Logout",
+      component: LogoutView,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: ProfileView,
+      meta: {
+        requiresAuth: true
+      },
+    }
     
   ]
+})
+
+router.beforeEach((to) => {
+
+  const logged = useUserStore().isAuthenticated;
+  const authRequired = to.meta["requiresAuth"] || false
+  const guestRequired = to.meta["requiresGuest"] || false
+
+  if (!logged && authRequired) {
+    return { name: "Login" }
+  }
+  if (logged && guestRequired) {
+    return { name: "Home" }
+  }
 })
 
 export default router
---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useUserStore } from "../stores/userStore";
+import { mapState } from "pinia";
+</script>
+
+<template>
+    <main>
+        <h1>Profile</h1>
+
+        
+    </main>
+</template>
+
+<script>
+
+export default {
+    name: "ProfileView",
+    data() {
+        return {
+
+        }
+    },
+    computed: {
+        ...mapState(useUserStore, {
+            error: (state) => state.requestData.error,
+            user: 'user',
+        })
+    },
+}
+
+</script>
