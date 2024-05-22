32e9cedea9d33f1e1e8156418321b42ed49ee880
ilyaskalimullinn <ilyaskalimullinn@yandex.ru>
2024-05-22 14:48:35 +0300

    add home page

--- frontend/src/services/consts.js
+++ frontend/src/services/consts.js
@@ -1,1 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1'
+export const LESSONS_URL = 'https://www.youtube.com/@urokimeditacii'
+export const TESTS_URL = 'https://skillbox.ru/media/growth/psychological-tests/'
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,14 +4,32 @@
 <template>
   <MainLayout>
     <h1>Home page</h1>
+    <h2>Начните работу с собой прямо сейчас</h2>
+    <div>
+      <p>Воспользуйтесь фильтром поиском для подбора психолога, который поймет вас</p>
+      <RouterLink :to="{name: 'SpecialistSearch'}">Начать</RouterLink>
+    </div>
+    <h2>Дополнительные материалы для самостоятельной работы:</h2>
+    <ul>
+      <li><a :href="lessons_url">Уроки медитаций</a></li>
+      <li><a :href="tests_url">Психологические тесты</a></li>
+    </ul>
   </MainLayout>
 </template>
 
 <script>
 import MainLayout from "../components/blocks/MainLayout.vue";
+import { LESSONS_URL, TESTS_URL } from "../services/consts.js";
+
 
 export default {
   name: "HomeView",
-  components: {MainLayout}
+  components: {MainLayout},
+  data() {
+    return {
+      lessons_url: LESSONS_URL,
+      tests_url: TESTS_URL
+    }
+  }
 }
 </script>
